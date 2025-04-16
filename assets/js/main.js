fc75e15f429445e9ea08c0bee2cb6df1c788fad2
hariswara <hariswaranvenkatesh@gmail.com>
2025-04-16 22:20:05 +0530

    LocalStorage-processing

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,10 +21,23 @@ validateForm.addField("#text-area", [
   { rule: "required", errorMessage: "Date and time is required" },
 ]);
 
-validateForm.onSuccess((e) => {
-  e.preventDefault();
+validateForm.onSuccess(() => {
   const formData = new FormData(formEl);
-  for (let [key, value] of formData.entries()) {
-    console.log(`${key}: ${value}`);
-  }
+
+  // for (let [key, value] of formData.entries()) {
+  //   console.log(`${key}: ${value}`);
+  // }
+
+  const formValueObj = Object.fromEntries(formData.entries());
+
+  //Get existing LocalStorage value, it it's exist!
+  const existingCourierData = localStorage.getItem("courierData");
+  // console.log(existingCourierData);
+
+  //Parse that string into Javascript value
+  const existingCourierArray = JSON.parse(existingCourierData);
+  console.log(existingCourierArray);
+
+  //Create a new array and push the existing LocalStorage value into new array
+  //Push the new array (which has all the info fo the LocalStorage)
 });
